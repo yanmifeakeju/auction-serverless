4b5488d0197f1bd0ad9990ae983c0f53cc1d5851
yannifeakeju <yanmifeakeju@gmail.com>
2023-03-01 21:04:48 +0100

    Dynamodb scan

--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -1,36 +1,27 @@
-import { v4 as uuid } from 'uuid';
-import { putItem } from '../libs/dynamoCommand.js';
 import middy from '@middy/core';
 import httpJsonBodyParser from '@middy/http-json-body-parser';
 import httpEventNormalizer from '@middy/http-event-normalizer';
 import httpErrorHandler from '@middy/http-error-handler';
 import createHttpError from 'http-errors';
+import { scanTableItems } from '../libs/dynamoCommand.js';
 
-async function createAuction(event, _context) {
-  const { title } = event.body;
-  const now = new Date();
-
-  const auction = {
-    id: uuid(),
-    title,
-    status: 'OPEN',
-    createdAt: now.toISOString(),
-  };
+async function getAuctions(event, _context) {
+  let auctions;
 
   try {
-    await putItem(process.env.AUCTIONS_TABLE_NAME, auction);
+    const result = await scanTableItems(process.env.AUCTIONS_TABLE_NAME);
+    auctions = result.Items || [];
   } catch (error) {
     console.log(error);
     throw new createHttpError.InternalServerError(error);
   }
-
   return {
-    statusCode: 201,
-    body: JSON.stringify({ auction }),
+    statusCode: 200,
+    body: JSON.stringify({ auctions }),
   };
 }
 
-export const handler = middy(createAuction)
+export const handler = middy(getAuctions)
   .use(httpJsonBodyParser())
   .use(httpEventNormalizer())
   .use(httpErrorHandler());
--- src/libs/dynamoCommand.js
+++ src/libs/dynamoCommand.js
@@ -1,5 +1,6 @@
-import { PutCommand } from '@aws-sdk/lib-dynamodb';
+import { PutCommand, ScanCommand } from '@aws-sdk/lib-dynamodb';
 import { ddbDocClient } from '../infrastructure/databases/dynamoDB/documentClient.js';
+import { ddbClient } from '../infrastructure/databases/dynamoDB/client.js';
 
 export const putItem = async (tableName, items) => {
   const params = {
@@ -9,4 +10,8 @@ export const putItem = async (tableName, items) => {
     },
   };
   return ddbDocClient.send(new PutCommand(params));
+};
+
+export const scanTableItems = async (tableName) => {
+  return ddbClient.send(new ScanCommand({ TableName: tableName }));
 };
