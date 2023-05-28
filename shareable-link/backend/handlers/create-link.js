45a20ab520971165fb3c2cd89126e20d23ec3c62
yanmifeakeju <yanmifeakeju@gmail.com>
2023-05-28 09:03:01 +0100

    middy and power tools

--- shareable-link/backend/handlers/create-link.js
+++ shareable-link/backend/handlers/create-link.js
@@ -1,3 +1,13 @@
+import {
+  Metrics,
+  logMetrics,
+  MetricUnits
+} from '@aws-lambda-powertools/metrics'
+import { Tracer, captureLambdaHandler } from '@aws-lambda-powertools/tracer'
+import { Logger, injectLambdaContext } from '@aws-lambda-powertools/logger'
+import middy from '@middy/core'
+import httpContentNegotiation from '@middy/http-content-negotiation'
+import httpHeaderNormalizer from '@middy/http-header-normalizer'
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 import { randomUUID } from 'node:crypto'
@@ -6,9 +16,13 @@ const { BUCKET_NAME, BASE_URL } = process.env
 const EXPIRY_DEFAULT = 24 * 60 * 60
 const MIME_TYPE = 'application/octet-stream'
 
+const tracer = new Tracer()
+const logger = new Logger()
+const metrics = new Metrics()
+
 const s3Client = new S3Client()
 
-export const handler = async (event, context) => {
+const createShareableLink = async (event, _context) => {
   const id = randomUUID()
   const key = `shareable/${id[0]}/${id[1]}/${id}`
 
@@ -17,6 +31,15 @@ export const handler = async (event, context) => {
   const contentDisposition = filename && `attachment; filename="${filename}"`
   const contentDispositionHeader =
     contentDisposition && `content-disposition: ${contentDisposition}`
+
+  logger.info('Creating share with the key', {
+    id,
+    key,
+    filename,
+    contentDispositionHeader
+  })
+
+  metrics.addMetric('shareable.link', MetricUnits.Count, 1)
 
   const downloadUrl = `${BASE_URL}/share/${id}`
 
@@ -34,19 +57,50 @@ export const handler = async (event, context) => {
     signableHeaders
   })
 
-  return {
-    statusCode: 201,
-    body: JSON.stringify({
+  let headers = {
+    'content-type': 'text/plain'
+  }
+
+  let body = `Upload with: curl -X PUT -T ${filename || '<FILENAME>'} ${
+    contentDispositionHeader ? `-H '${contentDispositionHeader}'` : ''
+  } '${uploadURL}'
+  |
+  Download with: curl ${downloadUrl}`
+
+  if (event.preferredMediaType === 'application/json') {
+    body = JSON.stringify({
       success: true,
       data: {
+        headers: [contentDispositionHeader],
         uploadURL,
         downloadUrl,
-        instruction: `
-        Upload with: curl -X PUT -T ${filename || '<FILENAME>'} ${
-          contentDispositionHeader ? `-H '${contentDispositionHeader}'` : ''
-        } '${uploadURL}'
-        `
+        instruction: body
       }
     })
+
+    headers = {
+      'content-type': 'application/json'
+    }
+  }
+
+  return {
+    statusCode: 201,
+    headers,
+    body
   }
 }
+
+export const handler = middy(createShareableLink)
+  .use(injectLambdaContext(logger, { logEvents: true }))
+  .use(logMetrics(metrics))
+  .use(captureLambdaHandler(tracer))
+  .use(httpHeaderNormalizer())
+  .use(
+    httpContentNegotiation({
+      parseCharsets: false,
+      parseEncodings: false,
+      parseLanguages: false,
+      failOnMismatch: false,
+      availableMediaTypes: ['text/plain', 'application/json']
+    })
+  )
--- shareable-link/backend/handlers/download.js
+++ shareable-link/backend/handlers/download.js
@@ -1,12 +1,24 @@
+import {
+  Metrics,
+  logMetrics,
+  MetricUnits
+} from '@aws-lambda-powertools/metrics'
+import { Tracer, captureLambdaHandler } from '@aws-lambda-powertools/tracer'
+import { Logger, injectLambdaContext } from '@aws-lambda-powertools/logger'
+import middy from '@middy/core'
 import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 
 const { BUCKET_NAME } = process.env
 const EXPIRY_DEFAULT = 300
 
+const tracer = new Tracer()
+const logger = new Logger()
+const metrics = new Metrics()
+
 const s3Client = new S3Client()
 
-export const handler = async (event, context) => {
+const downloadFileContent = async (event, context) => {
   const { id } = event.pathParameters
   const key = `shareable/${id[0]}/${id[1]}/${id}`
 
@@ -17,6 +29,9 @@ export const handler = async (event, context) => {
     expiresIn: EXPIRY_DEFAULT
   })
 
+  logger.info('Downloading share', { id, key })
+  metrics.addMetric('shareable.download', MetricUnits.Count, 1)
+
   return {
     statusCode: 301,
     headers: {
@@ -24,3 +39,8 @@ export const handler = async (event, context) => {
     }
   }
 }
+
+export const handler = middy(downloadFileContent)
+  .use(injectLambdaContext(logger, { logEvents: true }))
+  .use(logMetrics(metrics))
+  .use(captureLambdaHandler(tracer))
